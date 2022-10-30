51f9d3af4bc4ead71d324e7f461990c6e59dedcc
magusnigri <link_nigri@hotmail.com>
2022-10-29 21:25:49 -0300

    botao

--- assets/scrpit/index.js
+++ assets/scrpit/index.js
@@ -4,6 +4,7 @@ const menu = document.querySelector('.menu-lateral__content')
 
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral__content--ativo')
+    botaoMenu.classList.toggle('botao__menu-lateral--ativo')
 })
 
 
