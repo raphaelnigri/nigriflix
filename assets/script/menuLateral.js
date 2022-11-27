2a8ac44ca320479bd91948c6767033301cf47ef8
magusnigri <link_nigri@hotmail.com>
2022-11-27 01:23:22 -0300

    fecha menu clicando fora dele

--- assets/script/menuLateral.js
+++ assets/script/menuLateral.js
@@ -1,7 +1,17 @@
-const botaoMenu = document.querySelector('.botao__menu-lateral')
-const menu = document.querySelector('.menu-lateral__content')
+document.addEventListener('DOMContentLoaded',()=>{
+    const botaoMenu = document.querySelector('.botao__menu-lateral');
+    const menu = document.querySelector('.menu-lateral__content');
+    const main = document.getElementsByTagName('main');
 
-botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral__content--ativo')
-    botaoMenu.classList.toggle('botao__menu-lateral--ativo')
+    botaoMenu.addEventListener('click', () => {
+        menu.classList.toggle('menu-lateral__content--ativo');
+        botaoMenu.classList.toggle('botao__menu-lateral--ativo');
+    })
+
+    main[0].addEventListener('click',()=>{
+        if(menu.classList.contains('menu-lateral__content--ativo')){
+            menu.classList.remove('menu-lateral__content--ativo');
+            botaoMenu.classList.remove('botao__menu-lateral--ativo');
+        }
+    })
 })
